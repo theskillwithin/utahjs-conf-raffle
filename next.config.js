b2fb842c4b3da04dfb063af15dee8df486b96a3f
theskillwithin <austinppeterson@gmail.com>
2020-03-19 15:11:39 -0600

    favicons, normalize

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
+const path = require('path')
 const withOffline = require('next-offline')
 
-const nextConfig = {}
+const nextConfig = {
+  webpack(config, options) {
+    config.resolve.alias['components'] = path.join(__dirname, 'components')
+    return config
+  },
+}
 
 module.exports = withOffline(nextConfig)
